f2d9c2097fe66e2dc35750ff81d8df0dae8b7f8e
SBhanwala <rsingh.saurabh@gmail.com>
2023-10-03 20:54:02 +0530

    corrected function

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,18 @@
 
 
 print("adding all data processing code for fraud detection")
+
+a = 4
+b = 6
+
+def sum(a,b):
+    c = a + b
+    return c
+
+print("sum function")
+
+
+def multiplication(a,b):
+    c = a * b
+    return c
+print("mutplication function")
